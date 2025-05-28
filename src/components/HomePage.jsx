ae9f1ee44bfdfeefd4052f7b15f43a707047fdc9
Arantis_jr <arantisjunior@gmail.com>
2025-05-28 07:07:52 +0100

    Started with the mobile version of the todo web-app

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../styles/Homepage.css'
+
+const HomePage = () => {
+  return (
+  <>
+  <div className="home_page_main_container">
+    <div className="home_page_heeading">
+      
+    </div>
+  </div>
+  </>
+  )
+}
+
+export default HomePage
+
